a88ad1a3fdf88dd3c26db72c9eaf92012ed22a09
Marta Mohedano <139046036+mmohedano@users.noreply.github.com>
2023-10-22 13:26:21 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const listaPalos = ["♦", "♥", "♠", "♣"];
+const listaNumeros = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+let cartaEstado = { palo: listaPalos[0], numero: listaNumeros[0] };
+
+document.generarCarta = () => {
+  const palo = obtenerPaloAleatorio();
+  const numero = obtenerNumeroAleatorio();
+  cartaEstado = { palo, numero };
+  renderizar(cartaEstado);
+};
+
+const obtenerPaloAleatorio = () => {
+  const randomIndexPalo = Math.floor(Math.random() * listaPalos.length);
+  return listaPalos[randomIndexPalo];
+};
+
+const obtenerNumeroAleatorio = () => {
+  const randomIndexNumero = Math.floor(Math.random() * listaNumeros.length);
+  return listaNumeros[randomIndexNumero];
+};
+
+const renderizar = carta => {
+  console.log(carta);
+  const figuraSupIzq = document.querySelector(".sup-izq");
+  figuraSupIzq.innerHTML = carta.palo;
+  const figuraInfDer = document.querySelector(".inf-der");
+  figuraInfDer.innerHTML = carta.palo;
+  const num = document.querySelector(".num");
+  num.innerHTML = carta.numero;
 };
